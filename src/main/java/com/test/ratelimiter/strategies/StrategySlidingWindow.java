a9ff6d80f9b9f47f457ba7a0004c5bc884b58db2
Deemone <awaitingforsunrise@gmail.com>
2025-07-11 15:29:17 +0300

    Correct working with Genetics

--- src/main/java/com/test/ratelimiter/strategies/StrategySlidingWindow.java
+++ src/main/java/com/test/ratelimiter/strategies/StrategySlidingWindow.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Queue;
 
 
-//Also could be done in more optimal way with HashMap<Key, Queue<Timestamp>>
+//Also could be done in more optimal way with HashMap<Key, Queue<Timestamp>> or using ZSET in Redis
 public class StrategySlidingWindow<T> implements RateLimiterStrategyInterface<T> {
 
     private final HashMap<FilterField<T>, Integer> requestCounter = new HashMap<>();
