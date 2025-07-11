cc093fbfc252a54841b93087657a564cd430f0f4
Deemone <awaitingforsunrise@gmail.com>
2025-07-11 15:28:04 +0300

    Correct working with Genetics

--- src/main/java/com/test/ratelimiter/strategies/StrategySlidingWindow.java
+++ src/main/java/com/test/ratelimiter/strategies/StrategySlidingWindow.java
@@ -9,18 +9,9 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 
+
+//Also could be done in more optimal way with HashMap<Key, Queue<Timestamp>>
 public class StrategySlidingWindow<T> implements RateLimiterStrategyInterface<T> {
-
-    /*
-    We need 2 data structures: hashmap with <IP, request_count> and Queue with map of <Timestamp, Key>.
-    When client calls our service:
-     1. We increment the amount in HashMap by 1
-     2. We pop the Queue until the value fits in sliding window shows.
-     3. As soon as the pop from the Queue is in the progress, we decrement the values from the HashMap
-     4. When it's done, we check the final value
-
-     Also we could do it with HashMap<Key, Queue<Timestamp>>, similar to Redis's ZSET
-     */
 
     private final HashMap<FilterField<T>, Integer> requestCounter = new HashMap<>();
     private final Queue<Map<FilterField<T>, Instant>> requestQueue = new LinkedList<>();
